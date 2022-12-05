fae732582a79899485f8c6b3c15bba9fa75933d9
MorveN11 <morales.patty.jose@gmail.com>
2022-12-05 15:13:22 -0400

    init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'prettier',
+    'standard-jsx'
+  ],
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': ['warn'],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/prop-types': 'off',
+    'max-len': [1, { code: 100 }],
+    'comma-dangle': ['warn'],
+    'space-before-function-paren': 0,
+    'multiline-ternary': 0,
+    'react/jsx-curly-newline': 0,
+    camelcase: 0,
+    semi: 0,
+    'react/jsx-first-prop-new-line': 0,
+    'react/jsx-indent-props': 0,
+    'no-multi-str': 0
+  }
+};
