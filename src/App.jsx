3cd6f72fe991f6bdacd25231cc04734a41861512
MorveN11 <morales.patty.jose@gmail.com>
2022-12-14 00:10:35 -0400

    add global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
+import { AppContextProvider } from './context/AppProvider';
 import Login from './pages/Login';
-import NotFound from './pages/NotFound';
 import SignUp from './pages/SignUp';
+import LoadLogin from './pages/LoadLogin';
+import LoadSignUp from './pages/LoadSignUp';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <div className='bg-[#b8bec3] flex flex-col h-screen items-center'>
-      <Routes>
-        <Route path='/login/:idApp' element={<Login />} />
-        <Route path='/signup/:idApp' element={<SignUp />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <AppContextProvider>
+        <Routes>
+          <Route path='/login/:idApp' element={<Login />} />
+          <Route path='/signup/:idApp' element={<SignUp />} />
+          <Route path='/login' element={<LoadLogin />} />
+          <Route path='/signup' element={<LoadSignUp />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </AppContextProvider>
     </div>
   );
 }
